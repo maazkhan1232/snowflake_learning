31961c0d7f3bc5cb1b4eb55b06f93e1b7be1ccfd
Coca Beans <66106872+maazkhan1232@users.noreply.github.com>
2024-01-09 11:17:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,18 @@
 import streamlit as st
 st.write('App')
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+#load data into a DataFrame object:
+df = pd.DataFrame(data)
+
+if 'view_state' not in st.session_state:
+  st.session_state.view_state = False
+
+view_button = st.button('View')
+
+if view_button or st.session_state.view_state:
+  edited_data = st.data_editor(df)
+  st.write(st.session_state)
